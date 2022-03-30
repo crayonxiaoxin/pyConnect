85a020f371576f18f1d0fd3367255e8fb5eb860a
Lau <lau@efortunetech.com>
2022-03-30 14:02:08 +0800

    优化代码

--- Connect/spiders/zgw.py
+++ Connect/spiders/zgw.py
@@ -15,7 +15,7 @@ class ZgwSpider(scrapy.Spider):
         print(urls)
         for url in urls:
             # 中国网
-            if url.find("//news.china.com.cn/") != -1:
+            if url.find("china.com.cn/") != -1:
                 yield scrapy.Request(url, callback=self.parse_china_com_cn, cookies=None, cb_kwargs={"url": url})
             # 中华网
             if url.find("//news.china.com/") != -1:
@@ -28,23 +28,44 @@ class ZgwSpider(scrapy.Spider):
     def parse_china_com_cn(self, response, url):
         # http://news.china.com.cn/2022-03/29/content_78135389.htm
         title = response.xpath('//h1[@class="articleTitle"]/text()').get()
+        # http://henan.china.com.cn/m/2022-03/29/content_41919531.html
+        title_mobile = response.xpath('//div[contains(@class,"d_title")]/text()').get()
         if title is not None:
-            content = response.xpath('//div[@id="articleBody"]').get()
-            pubtime = response.xpath('//span[@id="pubtime_baidu"]/text()').get()
-            pubtime = pubtime.replace('发布时间：', '')
-            source = response.xpath('//span[@id="source_baidu"]/a/text()').get()
-            author = response.xpath('//span[@id="author_baidu"]/text()').get()
-            author = author.replace('作者：', '')
-            item = NewsItem()
-            item['title'] = title
-            item['content'] = str(content)
-            item['pub_time'] = pubtime
-            item['author'] = author
-            item['source'] = source
-            item['url'] = url
-            item['origin'] = "中国网"
-            if content is not None:
-                yield item
+            yield from self.parse_china_com_cn_web(response, title, url)
+        elif title_mobile is not None:
+            yield from self.parse_china_com_cn_mobile(response, title_mobile, url)
+
+    # 中国网 - mobile
+    def parse_china_com_cn_mobile(self, response, title_mobile, url):
+        item = NewsItem()
+        item['title'] = title_mobile
+        content = response.xpath('//div[contains(@class,"d_img")]').get()
+        item['content'] = str(content)
+        pubtime = response.xpath('//div[contains(@class,"d_time")]/span/text()').get()
+        item['pub_time'] = pubtime.strip()
+        source = response.xpath('//div[contains(@class,"d_time")]/text()').get()
+        item['source'] = source.strip().replace('来源：', '')
+        item['author'] = ""
+        item['url'] = url
+        item['origin'] = "中国网"
+        if content is not None:
+            yield item
+
+    # 中国网 - web
+    def parse_china_com_cn_web(self, response, title, url):
+        item = NewsItem()
+        item['title'] = title
+        content = response.xpath('//div[@id="articleBody"]').get()
+        item['content'] = str(content)
+        pubtime = response.xpath('//span[@id="pubtime_baidu"]/text()').get()
+        item['pub_time'] = pubtime.replace('发布时间：', '')
+        item['source'] = response.xpath('//span[@id="source_baidu"]/a/text()').get()
+        author = response.xpath('//span[@id="author_baidu"]/text()').get()
+        item['author'] = author.replace('作者：', '')
+        item['url'] = url
+        item['origin'] = "中国网"
+        if content is not None:
+            yield item
 
     # 中华网
     def parse_china_com(self, response, url):
@@ -57,20 +78,18 @@ class ZgwSpider(scrapy.Spider):
         elif title_type2 is not None:
             yield from self.parse_china_com_type_2(response, title_type2, url)
 
+    # 中华网 - 类型1
     def parse_china_com_type_1(self, response, title_type1, url):
-        content = response.xpath('//div[@id="chan_newsDetail"]').get()
-        pubtime = response.xpath('//div[@id="chan_newsInfo"]/text()[3]').get()
-        pubtime = pubtime.strip().replace("&nbsp;", "")
-        source = response.xpath('string(//span[@class="chan_newsInfo_source"])').get()
-        source = source.strip()
-        author = response.xpath('string(//span[@class="chan_newsInfo_author"])').get()
-        author = author.strip()
         item = NewsItem()
         item['title'] = title_type1
+        content = response.xpath('//div[@id="chan_newsDetail"]').get()
         item['content'] = str(content)
-        item['pub_time'] = pubtime
-        item['author'] = author
-        item['source'] = source
+        pubtime = response.xpath('//div[@id="chan_newsInfo"]/text()[3]').get()
+        item['pub_time'] = pubtime.strip().replace("&nbsp;", "")
+        source = response.xpath('string(//span[@class="chan_newsInfo_source"])').get()
+        item['source'] = source.strip()
+        author = response.xpath('string(//span[@class="chan_newsInfo_author"])').get()
+        item['author'] = author.strip()
         item['url'] = url
         item['origin'] = "中华网"
         next_link = response.xpath('//a[@class="allPage"]/@href').get()
@@ -93,19 +112,17 @@ class ZgwSpider(scrapy.Spider):
             print(item)
             yield item
 
+    # 中华网 - 类型2
     def parse_china_com_type_2(self, response, title_type1, url):
-        content = response.xpath('//div[contains(@class,"article_content")]').get()
-        pubtime = response.xpath('//div[contains(@class,"article_info")]/span[@class="time"]/text()').get()
-        pubtime = pubtime.strip().replace("&nbsp;", "")
-        source = response.xpath('//div[contains(@class,"article_info")]/span[@class="source"]/a/text()').get()
-        source = source.strip()
-        author = ""
         item = NewsItem()
         item['title'] = title_type1
+        content = response.xpath('//div[contains(@class,"article_content")]').get()
         item['content'] = str(content)
-        item['pub_time'] = pubtime
-        item['author'] = author
-        item['source'] = source
+        pubtime = response.xpath('//div[contains(@class,"article_info")]/span[@class="time"]/text()').get()
+        item['pub_time'] = pubtime.strip().replace("&nbsp;", "")
+        source = response.xpath('//div[contains(@class,"article_info")]/span[@class="source"]/a/text()').get()
+        item['source'] = source.strip()
+        item['author'] = ""
         item['url'] = url
         item['origin'] = "中华网"
         next_link = response.xpath('//a[@class="allPage"]/@href').get()
@@ -132,8 +149,12 @@ class ZgwSpider(scrapy.Spider):
     def parse_dbw_cn(self, response, url):
         title = response.xpath('//div[@id="end-box"]/h1[1]/text()').get()
         if title is not None:
+            item = NewsItem()
+            item['title'] = title
             content = response.xpath('//div[@class="zhengw"]').get()
+            item['content'] = str(content)
             pubtime = response.xpath('//div[@class="time"]/text()[1]').get()
+            item['pub_time'] = pubtime.strip()
             author_source = response.xpath('//span[@class="rl"]/text()').get()
             author_source = str(author_source).split(" 　")
             if len(author_source) == 2:
@@ -142,10 +163,6 @@ class ZgwSpider(scrapy.Spider):
             else:
                 author = ""
                 source = ""
-            item = NewsItem()
-            item['title'] = title
-            item['content'] = str(content)
-            item['pub_time'] = pubtime.strip()
             item['author'] = author
             item['source'] = source
             item['url'] = url
